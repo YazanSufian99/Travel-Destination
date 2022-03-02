20393b0e723bb304d9084f86a4d6e05eef4587c6
YazanSufian99 <yazan.sufian@yahoo.com>
2022-03-02 15:13:47 +0300

    editstrc

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import Tours from "../tours/Tours";
-import data from ".../data/data.json"
+import data from "../data/data.json"
 function App(){
     return(
         <>
